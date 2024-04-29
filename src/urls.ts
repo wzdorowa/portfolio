baa441f1946e294ccfcf8add73a4b928f65728ee
wzdorowa <w.zdorowa@gmail.com>
2024-04-29 20:36:37 +0300

    Update APOSTRO url

--- src/urls.ts
+++ src/urls.ts
@@ -3,4 +3,4 @@ export const VK_URL = "https://vk.com/serdcenika";
 export const CERT = "https://metalamp.ru/cert";
 export const METALAMP = "https://metalamp.ru/";
 export const ONLY = "https://only.digital/";
-export const APOSTRO = "https://app.apostro.xyz/";
+export const APOSTRO = "https://www.apostro.xyz/";
