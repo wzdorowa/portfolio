0ad1350a445002182e4ee420bcaf766de4d4e95f
wzdorowa <w.zdorowa@gmail.com>
2025-10-20 13:01:16 +0300

    Minor fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="ru">
+    <html lang="ru" style={{ scrollBehavior: "smooth" }}>
       <body className={`${montserrat.className}`}>
         <ThemeRegistry>{children}</ThemeRegistry>
       </body>
